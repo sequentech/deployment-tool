37dbcf65077c1d094d087539bf704acbb7cf07e5
ruescasd <fastness@gmail.com>
2015-01-19 00:23:58 +0100

    passing configuration location in run.sh

--- agora-elections/templates/run.sh
+++ agora-elections/templates/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/agoraelections/agora-elections/target/universal/stage/bin/agora-elections -v
+/home/agoraelections/agora-elections/target/universal/stage/bin/agora-elections -v -Dconfig.file=/home/agoraelections/agora-elections/conf/application.conf
