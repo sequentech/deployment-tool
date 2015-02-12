04aff7296b43a1ee77058ede9c63923ac4475eeb
Daniel Garcia Moreno <danigm@wadobo.com>
2015-02-12 12:09:24 +0100

    Fixing non-ascii chars error in results.sh

--- agora-elections/templates/results.sh
+++ agora-elections/templates/results.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # ./results.sh -t tally.tar.gz -c config.json -s
+
+# fixes errors on non-ascii characters
+declare -x LANG="en_GB.UTF-8"
+declare -x LANGUAGE="en_GB:en"
+
 AGORA_RESULTS=/home/agoraelections/agora-results
 VENV=/home/agoraelections/renv
 source $VENV/bin/activate
