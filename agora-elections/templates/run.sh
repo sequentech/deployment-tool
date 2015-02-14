a14df75cd400e59236020c28fc69b8c825f6583c
Eduardo Robles Elvira <edulix@wadobo.com>
2015-02-14 12:15:39 +0100

    fix run agora-elections

--- agora-elections/templates/run.sh
+++ agora-elections/templates/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+PID_FILE=/home/agoraelections/agora-elections/target/universal/stage/RUNNING_PID
+if [ -f "$PID_FILE" ]
+then
+    kill -9 $(cat "$PID_FILE")
+    rm "$PID_FILE"
+fi
+
+
 # fixes errors on non-ascii characters
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
