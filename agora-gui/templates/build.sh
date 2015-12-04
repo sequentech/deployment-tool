dbe62cfc33b32b2537ebcc33d9bfd85103add132
Eduardo Robles Elvira <edulix@agoravoting.com>
2015-12-04 17:33:53 +0100

    making atomic builds of agoragui really work

--- agora-gui/templates/build.sh
+++ agora-gui/templates/build.sh
@@ -2,19 +2,38 @@
 base=/home/agoragui
 guib=/home/agoragui
 
-# compile all the modules
-
+# compile all the modules, one by one. stop if they don't build, because
+# otherwise we would put in production a non-working version of the software
 cd $guib/agora-gui-admin/
-grunt build || (echo "build error in agora-gui-admin" && exit 1)
+grunt build
+if [ $? -ne 0 ]
+then
+  echo "build error in agora-gui-admin"
+  exit 1
+fi
 
 cd $guib/agora-gui-booth/
-grunt build || (echo "build error in agora-gui-booth" && exit 1)
+grunt build
+if [ $? -ne 0 ]
+then
+  echo "build error in agora-gui-booth"
+  exit 1
+fi
 
 cd $guib/agora-gui-elections/
-grunt build || (echo "build error in agora-gui-elections" && exit 1)
+grunt build
+if [ $? -ne 0 ]
+then
+  echo "build error in agora-gui-elections"
+  exit 1
+fi
 
 # only switch to the new build if everything was built correctly
+[ -d $base/dist-admin ] && rm -rf $base/dist-admin
+cp -r $guib/agora-gui-admin/dist $base/dist-admin
 
-rm -rf $base/dist-admin && cp -r guib/agora-gui-admin/dist $base/dist-admin
-rm -rf $base/dist-booth && cp -r guib/agora-gui-booth/dist $base/dist-booth
-rm -rf $base/dist-elections && cp -r guib/agora-gui-elections/dist $base/dist-elections
+[ -d $base/dist-booth ] && rm -rf $base/dist-booth
+cp -r $guib/agora-gui-booth/dist $base/dist-booth
+
+[ -d $base/dist-elections ] && rm -rf $base/dist-elections
+cp -r $guib/agora-gui-elections/dist $base/dist-elections
