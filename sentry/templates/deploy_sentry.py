faf83b49057a8be2637bb277c001a513d1971d93
Eduardo Robles Elvira <edulix@agoravoting.com>
2015-12-04 02:46:05 +0100

    making sentry-book idempotent

---
+++ sentry/templates/deploy_sentry.py
@@ -0,0 +1,75 @@
+# sentry
+
+# call to this function to update the globs globals variable: update(globals())
+def update(globs):
+    globs['RAVEN_CONFIG'] = dict(dsn='{{ dsn_contents.stderr }}')
+
+    if 'raven.contrib.django.raven_compat' not in globs['INSTALLED_APPS']:
+        globs['INSTALLED_APPS'] = globs['INSTALLED_APPS'] + (
+            'raven.contrib.django.raven_compat',
+        )
+
+{% if config.sentry_msg_log %}
+    globs['ADMINS'] = ( ('msg log', '{{ config.sentry_msg_log_email }}'), )
+    globs['SERVER_EMAIL'] = '{{ config.authapi_server_email }}'
+{% endif %}
+
+    globs['LOGGING'] = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+{% if config.sentry_msg_log %}
+        'mail_admins': {
+            'level': 'INFO',
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+{% endif %}
+        'sentry': {
+            'level': 'INFO',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'level': 'ERROR',
+            'handlers': ['sentry'],
+            'propagate': False,
+        },
+        'authapi': {
+            'level': 'INFO',
+            'handlers': ['sentry'],
+            'propagate': False,
+        },
+{% if config.sentry_msg_log %}
+        'authapi.notify': {
+            'level': 'INFO',
+            'handlers': ['sentry', 'mail_admins'],
+            'propagate': False,
+        },
+{% endif %}
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
