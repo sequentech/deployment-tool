7899f0de6d91b0f55d13f79dcf5e7792b6e48e36
Eduardo Robles Elvira <edulix@agoravoting.com>
2015-12-07 13:43:53 +0100

    initial work load balancing

---
+++ halb/templates/syncdirs_from_master.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PIDFILE=/var/run/syncdirs.pidfile
+
+trap "{ [ -f $PIDFILE ] && kill -9 $(cat $PIDFILE) }" SIGINT SIGTERM
+
+while true
+do
+  rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" agoraelections@{{ config.load_balancing.slave.master_hostname }}:/home/agoraelections/datastore/ /home/agoraelections/datastore/
+  sleep {{ config.load_balancing.slave.rsync_update_secs }}
+done
