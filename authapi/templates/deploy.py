11d5fb61e925c2aa033548fbd5f76f70a54eebf4
Eduardo Robles Elvira <edulix@agoravoting.com>
2016-01-23 10:40:00 +0100

    adding more authapi config into config.yml

--- authapi/templates/deploy.py
+++ authapi/templates/deploy.py
@@ -21,3 +21,36 @@ DATABASES = {
 SHARED_SECRET = b'{{config.agora_elections.shared_secret}}'
 
 SECRET_KEY = '{{ config.global_secret_key }}'
+
+{% if config.authapi.sms.enabled %}
+SMS_PROVIDER = "{{config.authapi.sms.provider}}"
+
+SMS_DOMAIN_ID = "{{config.authapi.sms.domain_id}}"
+
+SMS_LOGIN = "{{config.authapi.sms.login}}"
+
+SMS_PASSWORD = "{{config.authapi.sms.password}}"
+
+SMS_URL = "{{config.authapi.sms.url}}"
+
+SMS_SENDER_ID = "{{config.authapi.sms.sender_id}}"
+
+SMS_VOICE_LANG_CODE = {{config.authapi.sms.voice_lang_code}}
+
+SMS_BASE_TEMPLATE = "{{config.authapi.sms.base_template}}"
+
+SMS_AUTH_CODE_URL = "https://{{ config.agora_elections.domain }}/election/%(authid)s/public/login"
+
+{% endif %}
+
+{% if config.authapi.email.enabled %}
+DEFAULT_FROM_EMAIL = "{{config.authapi.email.default_from_email}}"
+
+EMAIL_HOST = "{{config.authapi.email.email_host}}"
+
+EMAIL_PORT = {{config.authapi.email.email_port}}
+
+EMAIL_BASE_TEMPLATE = "{{config.authapi.email.base_template}}"
+
+EMAIL_AUTH_CODE_URL = "https://{{ config.agora_elections.domain }}/election/%(authid)s/public/login/%(email)s"
+{% endif %}
