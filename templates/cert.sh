149b72370a40a5a83e7d34be0edbb6e16f9ce217
Eduardo Robles Elvira <edulix@agoravoting.com>
2016-01-29 23:12:04 +0100

    make the cert.sh cert lifetime configurable - missed to add the cert.sh change in last commit

--- templates/cert.sh
+++ templates/cert.sh
@@ -14,7 +14,7 @@ CERT_PATH=/srv/certs/selfsigned/cert.pem
 
 if [ ! -f $CERT_PATH ] || [ $(md5sum $CERT_PATH | grep 72a8ee2b40e77cd6e77e1db9285c7e19 | wc -l) == "1" ]
 then
-  openssl req -nodes -x509 -newkey rsa:4096 -keyout key-nopass.pem -out cert.pem -days 3650 <<EOF
+  openssl req -nodes -x509 -newkey rsa:4096 -keyout key-nopass.pem -out cert.pem -days {{config.cert.lifetime}} <<EOF
 ${C}
 ${ST}
 ${L}
