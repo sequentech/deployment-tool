1b28c18082da6f3fe2485e0d8638e58a8d1b020f
Eduardo Robles Elvira <edulix@nvotes.com>
2017-09-04 08:30:45 +0200

    starting to add configurable admin gui questions options

--- agora-gui/templates/avConfig.js
+++ agora-gui/templates/avConfig.js
@@ -87,6 +87,24 @@ var avConfigData = {
     {% endfor %}
   ],
 
+  // This is the list of admin question layouts shown in the administrative
+  // user interface. If the list is empty, question layouts will not be shown
+  // as a configurable option
+  shownAdminQuestionLayouts: [
+    {% for item in config.agora_gui.shown_admin_question_layouts %}
+    "{{ item  }}"{% if not loop.last %},{% endif %}
+    {% endfor %}
+  ],
+
+  // This is the list of admin question voting systems shown in the
+  // administrative user interface. If the list is empty, question layouts
+  // will not be shown as a configurable option
+  shownAdminQuestionVotingSystems: [
+    {% for item in config.agora_gui.shown_admin_question_voting_systems %}
+    "{{ item  }}"{% if not loop.last %},{% endif %}
+    {% endfor %}
+  ],
+
   // admin fields
   adminFields: [
       {% for field in config.agora_gui.admin_fields %}
