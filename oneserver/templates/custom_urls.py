fdd2de6a202ee9d0e0b29a6b2f6bbdb3154503ae
Eduardo Robles Elvira <edulix@nvotes.com>
2020-02-06 12:38:54 +0100

    fix typo in import

--- oneserver/templates/custom_urls.py
+++ oneserver/templates/custom_urls.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with agora-dev-box.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls import include, path
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
