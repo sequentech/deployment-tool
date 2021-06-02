43204d500b7c0b002805d294d738ef1b5fccfa39
Eduardo Robles Elvira <edulix@nvotes.com>
2021-06-02 11:41:55 +0200

    iadding new config.yml setting to configure the auth token timeout, auth_token_expiration_seconds

--- authapi/templates/deploy.py
+++ authapi/templates/deploy.py
@@ -90,6 +90,8 @@ DATABASES = {
 
 SHARED_SECRET = b'{{config.agora_elections.shared_secret}}'
 
+TIMEOUT = {{config.auth_api.auth_token_expiration_seconds}}
+
 SECRET_KEY = '{{ config.global_secret_key }}'
 HOME_URL = "https://{{ config.agora_elections.domain }}/election/__EVENT_ID__/public/home"
 
