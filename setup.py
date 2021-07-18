0d872d533f130be00283320e3d4007548c7eba80
Eduardo Robles Elvira <edulix@nvotes.com>
2021-07-18 22:24:02 +0200

    make it work with ort

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# This file is part of agora-results.
+# Copyright (C) 2014-2016  Agora Voting SL <agora@agoravoting.com>
+
+# agora-results is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+
+# agora-results  is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with agora-results.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+setup(
+    name='agora-dev-box',
+    version='20.2.0',
+    author='Agora Voting SL',
+    author_email='contact@nvotes.com',
+    packages=[],
+    url='https://github.com/agoravoting/agora-dev-box/',
+    license='AGPL-3.0',
+    description='agora-dev-box',
+    long_description=open('README.md').read(),
+    install_requires=[]
+)
