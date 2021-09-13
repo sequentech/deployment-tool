f67e502239791920c1a41236f75c79c509dd4e4f
Eduardo Robles Elvira <edulix@gmail.com>
2021-09-13 08:57:33 +0200

    merging 4.0.0 back into master

--- helper-tools/config_prod_env.py
+++ helper-tools/config_prod_env.py
@@ -22,8 +22,8 @@ import argparse
 import re
 
 INPUT_PROD_VERSION="4.0.0"
-INPUT_PRE_VERSION="4.0.0"
-OUTPUT_PROD_VERSION="4.0.0"
+INPUT_PRE_VERSION="master"
+OUTPUT_PROD_VERSION="master"
 def store_keyvalue(prod_config, generated_config, keystore, pipe):
   '''
   Updates the keyvalue store
