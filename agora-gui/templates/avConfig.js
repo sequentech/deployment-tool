c1f686d360f51803938d21d1051db00192110224
Eduardo Robles Elvira <edulix@gmail.com>
2021-10-14 10:21:03 +0200

    Release for version 4.0.1

--- agora-gui/templates/avConfig.js
+++ agora-gui/templates/avConfig.js
@@ -20,7 +20,7 @@
  * in this same file, which you might want to edit and tune if needed.
  */
 
-var AV_CONFIG_VERSION = '4.0.0';
+var AV_CONFIG_VERSION = '4.0.1';
 
 var avConfigData = {
   // the base url path for ajax requests, for example for sending ballots or
--- helper-tools/config_prod_env.py
+++ helper-tools/config_prod_env.py
@@ -22,8 +22,8 @@ import argparse
 import re
 
 INPUT_PROD_VERSION="4.0.0"
-INPUT_PRE_VERSION="4.0.0"
-OUTPUT_PROD_VERSION="4.0.0"
+INPUT_PRE_VERSION="4.0.1"
+OUTPUT_PROD_VERSION="4.0.1"
 def store_keyvalue(prod_config, generated_config, keystore, pipe):
   '''
   Updates the keyvalue store
