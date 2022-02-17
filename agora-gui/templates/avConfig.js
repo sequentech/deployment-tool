a733e0cbaf26998c033ce13a5cfd4a2d51caa128
Eduardo Robles Elvira <edu@sequentech.io>
2022-02-17 17:17:52 +0000

    Release for version 5.0.9

--- agora-gui/templates/avConfig.js
+++ agora-gui/templates/avConfig.js
@@ -20,7 +20,7 @@
  * in this same file, which you might want to edit and tune if needed.
  */
 
-var AV_CONFIG_VERSION = '5.0.8';
+var AV_CONFIG_VERSION = '5.0.9';
 
 var avConfigData = {
   // the base url path for ajax requests, for example for sending ballots or
