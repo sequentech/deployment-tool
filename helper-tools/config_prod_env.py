a8a4cf9a5d38db1d7e4bc3bb0bb2c990da972487
Findeton <felix@sequentech.io>
2022-09-28 10:07:27 -0500

    Release for version 7.0.0

--- helper-tools/config_prod_env.py
+++ helper-tools/config_prod_env.py
@@ -21,9 +21,9 @@ import sys
 import argparse
 import re
 
-INPUT_PROD_VERSION="master"
-INPUT_PRE_VERSION="7.0.0-beta.1"
-OUTPUT_PROD_VERSION="7.0.0-beta.1"
+INPUT_PROD_VERSION="7.0.0-beta.1"
+INPUT_PRE_VERSION="7.0.0"
+OUTPUT_PROD_VERSION="7.0.0"
 def store_keyvalue(prod_config, generated_config, keystore, pipe):
   '''
   Updates the keyvalue store
