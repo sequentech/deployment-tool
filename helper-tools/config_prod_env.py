37db46ec91f79fd17d010a15977ddf37a9d73066
Eduardo Robles Elvira <edu@sequentech.io>
2023-10-19 17:02:59 +0200

    Release for version 10.0.2

--- helper-tools/config_prod_env.py
+++ helper-tools/config_prod_env.py
@@ -21,9 +21,9 @@ import sys
 import argparse
 import re
 
-INPUT_PROD_VERSION="10.0.0"
-INPUT_PRE_VERSION="10.0.1"
-OUTPUT_PROD_VERSION="10.0.1"
+INPUT_PROD_VERSION="10.0.1"
+INPUT_PRE_VERSION="10.0.2"
+OUTPUT_PROD_VERSION="10.0.2"
 def store_keyvalue(prod_config, generated_config, keystore, pipe):
   '''
   Updates the keyvalue store
