0dcdf930e9f79fb424addba821a4943d44b4b6c1
Findeton <felix@sequentech.io>
2023-11-01 10:44:37 -0500

    wip

--- sequent-ui/templates/install_node.sh
+++ sequent-ui/templates/install_node.sh
@@ -19,7 +19,7 @@ set -uex; \
     apt-get install -y ca-certificates curl gnupg; \
     mkdir -p /etc/apt/keyrings; \
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key \
-     | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg; \
+     | gpg --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg; \
     NODE_MAJOR=18; \
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" \
      > /etc/apt/sources.list.d/nodesource.list; \
