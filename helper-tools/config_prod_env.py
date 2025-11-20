b5b069e60d46c6af4f3fc7179f4c2f0ae5e85c74
Findeton <felix@sequentech.io>
2025-11-20 09:58:08 -0500

    Release for version 10.5.5

--- helper-tools/config_prod_env.py
+++ helper-tools/config_prod_env.py
@@ -21,9 +21,9 @@ import sys
 import argparse
 import re
 
-INPUT_PROD_VERSION="10.5.3"
-INPUT_PRE_VERSION="10.5.4"
-OUTPUT_PROD_VERSION="10.5.4"
+INPUT_PROD_VERSION="10.5.4"
+INPUT_PRE_VERSION="10.5.5"
+OUTPUT_PROD_VERSION="10.5.5"
 def store_keyvalue(prod_config, generated_config, keystore, pipe):
   '''
   Updates the keyvalue store
